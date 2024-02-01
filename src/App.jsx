2f4d5898e62447cbfe9b91f3a05e12a9b68225aa
garv190 <garv.jauhari@gmail.com>
2024-02-02 02:53:03 +0530

    Nothing shown{identify the problem}

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,83 @@
-import { useState } from 'react'
-import './App.css'
+
+
+
+import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from "./components/Navbar.jsx"
+import {  BrowserRouter as Router,Route,Routes } from "react-router-dom"
+import Slider from "./components/Slider.jsx"
+import data from "./data/data.json"
+import Offers from "./components/Offers.jsx"
+import Heading  from "./components/Heading.jsx"
+import StarProduct from "./components/StarProduct.jsx"
+import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.jsx"
+import HotAccessories from "./components/HotAccessories.jsx"
+import ProductReviews from "./components/ProductReviews.jsx"
+// import Videos from "./components/Videos.js"
+import Banner from "./components/Banner.jsx"
+import Footer from "./components/Footer.jsx"
+// import NavOptios from "./components/NavOptios.jsx"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Test push
-      </p>
-    </>
-  )
+  <Router>
+       <PreNavbar/>
+       <Navbar />
+            
+        <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+
+
+       <Slider start={data.banner.start} />
+       <Offers offer={data.offer} />
+       <Heading text="STAR PRODUCTS"/>
+       <StarProduct starProduct={data.starProduct} />
+       <Heading text="HOT ACCESSORIES" />
+       <HotAccessoriesMenu />
+
+       <Routes>
+
+<Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
+</Routes>
+            <Routes>
+       <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
+       </Routes>
+
+       <Routes>
+       <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
+       </Routes>
+
+       <Routes>
+       <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
+       </Routes>
+
+       <Routes>
+       <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
+       </Routes>
+
+
+
+  
+
+
+       <Heading text="PRODUCT REVIEWS"/>
+
+       <ProductReviews productReviews={data.productReviews}/>
+
+      <Heading  text="IN THE PRESS"/>
+   
+       <Banner  banner={data.banner}/>
+
+       <Footer footer={data.footer} />
+
+
+
+       
+   </Router>
+
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Offers.jsx
@@ -0,0 +1,18 @@
+
+import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+import React from 'react'
+import Offer from "./Offer.jsx"
+import "../styles/Offers.css"
+
+
+const Offers = ({offer}) => {
+    return (
+        <div className="offersSection"> 
+        {offer.map((item,index)=>(
+        <Offer key={item.image} index={index} src={item.image} link={item.url} />
+        ))}
+    </div>
+    )
+}
+
+export default Offers
