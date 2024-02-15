4cc5bc31be22fb9af4c87bd2b9396c1892da455c
KrishKoria <krishkoria2004@gmail.com>
2024-02-15 16:42:08 +0530

    slider fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,97 @@
-
-
-
-import './App.css';
-import Navbar from "./components/Navbar.jsx"
-import {  BrowserRouter as Router,Route,Routes } from "react-router-dom"
-import Slider from "./components/Slider.jsx"
-import data from "./data/data.json"
-import Offers from "./components/Offers.jsx"
-import Heading  from "./components/Heading.jsx"
-import StarProduct from "./components/StarProduct.jsx"
-import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.jsx"
-import HotAccessories from "./components/HotAccessories.jsx"
-import ProductReviews from "./components/ProductReviews.jsx"
-import Banner from "./components/Banner.jsx"
-import Footer from "./components/Footer.jsx"
-import NavOptios from "./components/NavOptios.jsx"
-
-
-
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Slider from "./components/Slider.jsx";
+import data from "./data/data.json";
+import Footer from "./components/Footer.jsx";
 function App() {
-
   return (
-  <Router>
-       <Navbar />
-            
-        <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+    <Router>
+      <Navbar />
 
-
-       <Slider start={data.banner.start} />
-       <Offers offer={data.offer} />
+      <Slider start={data.banner.start} />
+      {/* <Offers offer={data.offer} />
        <Heading text="STAR PRODUCTS"/>
        <StarProduct starProduct={data.starProduct} />
        <Heading text="HOT ACCESSORIES" />
-       <HotAccessoriesMenu />
+       <HotAccessoriesMenu /> */}
 
-       <Routes>
+      {/* <Routes>
+        <Route
+          exact
+          path="/music"
+          element={
+            <HotAccessories
+              music={data.hotAccessories.music}
+              musicCover={data.hotAccessoriesCover.music}
+            />
+          }
+        />
+      </Routes>
+      <Routes>
+        <Route
+          exact
+          path="/smartDevice"
+          element={
+            <HotAccessories
+              smartDevice={data.hotAccessories.smartDevice}
+              smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+            />
+          }
+        />
+      </Routes>
 
-<Route exact path="/music"  element = {<HotAccessories music = {data.hotAccessories.music} musicCover = {data.hotAccessoriesCover.music}/>}/>
-</Routes>
-            <Routes>
-       <Route exact path="/smartDevice" element={ <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice} />} />
-       </Routes>
+      <Routes>
+        <Route
+          exact
+          path="/home"
+          element={
+            <HotAccessories
+              home={data.hotAccessories.home}
+              homeCover={data.hotAccessoriesCover.home}
+            />
+          }
+        />
+      </Routes>
 
-       <Routes>
-       <Route exact path="/home" element={ <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home} />} />
-       </Routes>
+      <Routes>
+        <Route
+          exact
+          path="/lifestyle"
+          element={
+            <HotAccessories
+              lifestyle={data.hotAccessories.lifestyle}
+              lifestyleCover={data.hotAccessoriesCover.lifestyle}
+            />
+          }
+        />
+      </Routes>
 
-       <Routes>
-       <Route exact path="/lifestyle" element={ <HotAccessories lifestyle={data.hotAccessories.lifestyle} lifestyleCover={data.hotAccessoriesCover.lifestyle} />} />
-       </Routes>
+      <Routes>
+        <Route
+          exact
+          path="/mobileAccessories"
+          element={
+            <HotAccessories
+              mobileAccessories={data.hotAccessories.mobileAccessories}
+              mobileAccessoriesCover={
+                data.hotAccessoriesCover.mobileAccessories
+              }
+            />
+          }
+        />
+      </Routes> */}
 
-       <Routes>
-       <Route exact path="/mobileAccessories" element={ <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories} />} />
-       </Routes>
+      {/* <Heading text="PRODUCT REVIEWS" />
 
+      <ProductReviews productReviews={data.productReviews} />
 
+      <Heading text="IN THE PRESS" /> */}
 
-  
+      {/* <Banner banner={data.banner} /> */}
 
-
-       <Heading text="PRODUCT REVIEWS"/>
-
-       <ProductReviews productReviews={data.productReviews}/>
-
-      <Heading  text="IN THE PRESS"/>
-   
-       <Banner  banner={data.banner}/>
-
-       <Footer footer={data.footer} />
-
-
-
-       
-   </Router>
-
+      <Footer footer={data.footer} />
+    </Router>
   );
 }
 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,17 +3,15 @@ import Carousel from 'react-bootstrap/Carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const Slider = ({start}) => {
+const Slider = (props) => {
     return (
         <Carousel fade>
-       
-
-       {start.map((item)=>(
+       {props.start.map((item)=>(
             <Carousel.Item>
             <img
               className="d-block w-100"
               src={item}
-              alt="First slide"
+              alt="slide"
             />
           </Carousel.Item>
        ))}
