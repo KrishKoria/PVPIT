689603d5a933ca0b47af6c43e158a07e04e5c3cf
KrishKoria <krishkoria2004@gmail.com>
2024-02-15 22:30:03 +0530

    changed slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Image } from 'react-bootstrap';
 
 const Slider = (props) => {
-    return (
-        <Carousel fade>
-       {props.start.map((item)=>(
-            <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={item}
-              alt="slide"
-            />
-          </Carousel.Item>
-       ))}
-       
-      </Carousel>
-    )
-}
+  return (
+    <Carousel fade data-bs-theme="dark">
+      {props.start.map((item, index) => (
+        <Carousel.Item key={index}>
+          <Image
+            className="d-block w-100 carousel-image"
+            src={item}
+            alt={`slide-${index}`}
+            style={{ objectFit: 'contain', height: '500px'}}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
 
-export default Slider
+export default Slider;
