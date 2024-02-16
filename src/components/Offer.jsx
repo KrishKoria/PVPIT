caa5458318b7d765c9985af04d8316eb7c934c8a
KrishKoria <krishkoria2004@gmail.com>
2024-02-16 10:44:15 +0530

    Update Offer component and data.json

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -12,7 +12,7 @@ import {
 const Offer = ({src, description, title}) => {
     return (
         <MDBCard>
-          <MDBCardImage src={src} width={398} height={240} style={{objectFit: "fill"}}  position='top' alt='...' />
+          <MDBCardImage src={src} width={400} height={240} style={{objectFit: "contain"}}  position='top' alt='...' />
           <MDBCardBody>
             <MDBCardTitle>{title}</MDBCardTitle>
             <MDBCardText>
